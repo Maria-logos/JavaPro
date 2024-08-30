0a56d5da2cc77232c3a25e9dc2de88e4681f14b2
Maria <msinukova@gmail.com>
2024-08-30 17:34:56 +0300

    init commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,12 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        int[] numbers = new DataRepository().getData();
+        DataHandler dataHandler = new DataHandler();
+        for (int num : numbers) {
+            System.out.println("Initial value is " + num);
+            int newNum = dataHandler.modify(num);
+            System.out.println("New value is " + newNum);
+        }
     }
 }
